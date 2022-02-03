bb6c908476d9129e0b9ee0799dd428fdc361f6f2
Michael Desormeaux <53596354+M-Desormeaux@users.noreply.github.com>
2022-02-02 19:21:56 -0600

    new responses, base controllers complete (#20)

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -4,6 +4,8 @@ using LynkerSocial_API.Models;
 using LynkerSocial_API.ViewModels;
 using System.Threading;
 using System;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
 
 namespace LynkerSocial_API.Controllers
 {
@@ -19,29 +21,31 @@ namespace LynkerSocial_API.Controllers
             _db = db;
         }
 
+        [HttpGet("{userId}")]
+        public async Task<IActionResult> GetUser(Guid userId)
+        {
+            var user = await _db.Users.FindAsync(userId);
+
+            if (user == null)
+            {
+                return NotFound(ApiResponse.Failure("User not found"));
+            }
+
+            return Ok(ApiResponse<User>.Success(user));
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllUsers()
+        {
+            var userList = await _db.Users.ToListAsync();
+
+            return Ok(ApiResponse<IEnumerable<User>>.Success(userList));
+        }
+
         [HttpPost]
         public async Task<IActionResult> CreateUser(UserViewModel userModel, CancellationToken cancelToken)
         {
-            Response response = new Response()
-            {
-                Success = true,
-                Message = $"{userModel.Name} created."
-            };
-
-            // Prevalidates so this is obsolete
-
-            // if (!ModelState.IsValid)
-            // {
-            //     // Sanity control
-            //     response.Message = "Creation requirements not fulfilled";
-
-            //     // Grab all errors
-            //     // response.Message = string.Join(",", ModelState.SelectMany(x => x.Value.Errors).Select(x => x.ErrorMessage));
-
-            //     return BadRequest(response);
-            // }
-
-            User user = new User()
+            User user = new()
             {
                 Name = userModel.Name,
                 Birthday = userModel.Birthday
@@ -50,29 +54,24 @@ namespace LynkerSocial_API.Controllers
             _db.Users.Add(user);
             await _db.SaveChangesAsync(cancelToken);
 
-            return Ok(response);
+            return Ok(ApiResponse<Guid>.Success(user.Id));
         }
 
         [HttpDelete]
         public async Task<IActionResult> DeleteUser(Guid userId, CancellationToken cancelToken)
         {
-            Response response = new Response();
 
             User user = await _db.Users.FindAsync(userId);
 
             if (user is null)
             {
-                response.Message = "User not found";
-                return NotFound(response);
+                return NotFound(ApiResponse<User>.Failure("User not found"));
             }
-
-            response.Success = true;
-            response.Message = $"{user.Name} deleted.";
 
             _db.Users.Remove(user);
             await _db.SaveChangesAsync(cancelToken);
 
-            return Ok(response);
+            return Ok();
         }
     }
 }
