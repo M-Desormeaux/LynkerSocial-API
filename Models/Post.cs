2df91fb18ac5e6b7407c1f340b7a2421921a17a4
Michael Desormeaux <mdesormeaux96@gmail.com>
2022-02-04 09:20:15 -0600

    merge

--- Models/Post.cs
+++ Models/Post.cs
@@ -1,4 +1,5 @@
 using System;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace LynkerSocial_API.Models
 {
@@ -6,8 +7,10 @@ namespace LynkerSocial_API.Models
     {
         public Guid Id { get; set; } = Guid.NewGuid();
         public DateTime CreationDate { get; set; } = DateTime.Now;
-        public Guid UserId { get; set; }
+        public Guid? UserId { get; set; }
         public User User { get; set; }
+        public Guid CommunityId { get; set; }
+        public Community Community { get; set; }
         public string Title { get; set; }
         public string Body { get; set; }
         public int Score { get; set; }
